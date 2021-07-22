4dbe5112537e98116e5efd1e2daead12055140f0
maxim-savchuk <maxsavchuk1105@gmail.com>
2021-07-22 23:01:44 +0300

    1

--- src/index.js
+++ src/index.js
@@ -12,51 +12,72 @@
  * http://newsapi.org/v2/everything?q=cat&language=en&pageSize=5&page=1
  */
 
-import articlesTpl from './templates/articles.hbs';
+// import articlesTpl from './templates/articles.hbs';
 import './css/common.css';
 import NewsApiService from './js/news-service';
-import LoadMoreBtn from './js/components/load-more-btn';
 
 const refs = {
   searchForm: document.querySelector('.js-search-form'),
   articlesContainer: document.querySelector('.js-articles-container'),
+  loadMoreBtn: document.querySelector('[data-action="load-more"]'),
 };
-const loadMoreBtn = new LoadMoreBtn({
-  selector: '[data-action="load-more"]',
-  hidden: true,
-});
 const newsApiService = new NewsApiService();
 
 refs.searchForm.addEventListener('submit', onSearch);
-loadMoreBtn.refs.button.addEventListener('click', fetchArticles);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
 function onSearch(e) {
   e.preventDefault();
 
   newsApiService.query = e.currentTarget.elements.query.value;
-
-  if (newsApiService.query === '') {
-    return alert('Введи что-то нормальное');
-  }
-
-  loadMoreBtn.show();
   newsApiService.resetPage();
-  clearArticlesContainer();
-  fetchArticles();
+  newsApiService.fetchArticles().then(articles=>console.log(articles));
 }
 
-function fetchArticles() {
-  loadMoreBtn.disable();
-  newsApiService.fetchArticles().then(articles => {
-    appendArticlesMarkup(articles);
-    loadMoreBtn.enable();
-  });
+function onLoadMore() {
+  newsApiService.fetchArticles().then(articles=>console.log(articles));
 }
 
-function appendArticlesMarkup(articles) {
-  refs.articlesContainer.insertAdjacentHTML('beforeend', articlesTpl(articles));
-}
 
-function clearArticlesContainer() {
-  refs.articlesContainer.innerHTML = '';
-}
+
+// import LoadMoreBtn from './js/components/load-more-btn';
+
+
+// const loadMoreBtn = new LoadMoreBtn({
+//   selector: '[data-action="load-more"]',
+//   hidden: true,
+// });
+// const newsApiService = new NewsApiService();
+
+// loadMoreBtn.refs.button.addEventListener('click', fetchArticles);
+
+// function onSearch(e) {
+//   e.preventDefault();
+
+//   newsApiService.query = e.currentTarget.elements.query.value;
+
+//   if (newsApiService.query === '') {
+//     return alert('Введи что-то нормальное');
+//   }
+
+//   loadMoreBtn.show();
+//   newsApiService.resetPage();
+//   clearArticlesContainer();
+//   fetchArticles();
+// }
+
+// function fetchArticles() {
+//   loadMoreBtn.disable();
+//   newsApiService.fetchArticles().then(articles => {
+//     appendArticlesMarkup(articles);
+//     loadMoreBtn.enable();
+//   });
+// }
+
+// function appendArticlesMarkup(articles) {
+//   refs.articlesContainer.insertAdjacentHTML('beforeend', articlesTpl(articles));
+// }
+
+// function clearArticlesContainer() {
+//   refs.articlesContainer.innerHTML = '';
+// }
--- src/js/news-service.js
+++ src/js/news-service.js
@@ -1,3 +1,44 @@
+export default class NewsApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+
+  }
+
+  fetchArticles() {
+    const options = {
+      headers: {
+        Authorization: '4330ebfabc654a6992c2aa792f3173a3',
+      },
+    };
+    const url = `https://newsapi.org/v2/everything?q=${this.searchQuery}&language=en&pageSize=5&page=${this.page}`;
+
+    return fetch(url, options)
+      .then(r => r.json())
+      .then(data => {
+        this.incrementPage();
+        
+        return data.articles;
+      });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
+
 // const API_KEY = '4330ebfabc654a6992c2aa792f3173a3';
 // const BASE_URL = 'https://newsapi.org/v2';
 // const options = {
