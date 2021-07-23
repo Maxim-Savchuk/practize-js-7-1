2106bcc907e12b6b69a92fd3338d8a741ebd4a86
maxim-savchuk <maxsavchuk1105@gmail.com>
2021-07-23 21:38:40 +0300

    done

--- src/js/news-service.js
+++ src/js/news-service.js
@@ -1,3 +1,7 @@
+const API_KEY = '4330ebfabc654a6992c2aa792f3173a3';
+const BASE_URL = 'https://newsapi.org/v2';
+
+
 export default class NewsApiService {
   constructor() {
     this.searchQuery = '';
@@ -8,17 +12,17 @@ export default class NewsApiService {
   fetchArticles() {
     const options = {
       headers: {
-        Authorization: '4330ebfabc654a6992c2aa792f3173a3',
+        Authorization: API_KEY,
       },
     };
-    const url = `https://newsapi.org/v2/everything?q=${this.searchQuery}&language=en&pageSize=5&page=${this.page}`;
+    const url = `${BASE_URL}/everything?q=${this.searchQuery}&language=en&pageSize=5&page=${this.page}`;
 
     return fetch(url, options)
-      .then(r => r.json())
-      .then(data => {
+      .then(response => response.json())
+      .then(({articles}) => {
         this.incrementPage();
         
-        return data.articles;
+        return articles;
       });
   }
 
@@ -38,45 +42,3 @@ export default class NewsApiService {
     this.searchQuery = newQuery;
   }
 }
-
-// const API_KEY = '4330ebfabc654a6992c2aa792f3173a3';
-// const BASE_URL = 'https://newsapi.org/v2';
-// const options = {
-//   headers: {
-//     Authorization: API_KEY,
-//   },
-// };
-
-// export default class NewsApiService {
-//   constructor() {
-//     this.searchQuery = '';
-//     this.page = 1;
-//   }
-
-//   fetchArticles() {
-//     const url = `${BASE_URL}/everything?q=${this.searchQuery}&language=en&pageSize=5&page=${this.page}`;
-
-//     return fetch(url, options)
-//       .then(response => response.json())
-//       .then(({ articles }) => {
-//         this.incrementPage();
-//         return articles;
-//       });
-//   }
-
-//   incrementPage() {
-//     this.page += 1;
-//   }
-
-//   resetPage() {
-//     this.page = 1;
-//   }
-
-//   get query() {
-//     return this.searchQuery;
-//   }
-
-//   set query(newQuery) {
-//     this.searchQuery = newQuery;
-//   }
-// }
